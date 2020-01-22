bea74d2918200f4d491ad2a6f57b5405c56a405b
Charlotte Hetzler <charlottehetzler@Admins-MBP.fritz.box>
2020-01-22 16:22:15 +0100

    first try to filter

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 import React from 'react';
 import './App.css';
-import { objectTypeAnnotation } from '@babel/types';
+// import { objectTypeAnnotation } from '@babel/types';
 
-var sampleData = require('./construction_shop_data.json');
+const sampleData = require('./construction_shop_data.json');
+const data = sampleData;
 
-function App(props) {
-  const data = sampleData;
-  let customers = data.customers;
-  let products = data.products;
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Ronzos WS</p>
-      </header>
+
+class CustomersTable extends React.Component {  
+  render() {
+    const customers = data.customers;
+    return (
       <div>
         <table>
           <thead>
-            <tr> 
-              {Object.keys(customers[0]).map(header => {return <td><p><strong>{header}</strong></p></td>})}
+            <tr>
+              <td><p>Name</p></td>
+              <td>
+                <p>Memership Status</p>
+              </td>
+              <td><p>Customer Years</p></td>
+              <td><p>Top Products</p></td>
             </tr>
           </thead>
           <tbody>
-            {customers.map(customer => {return <tr>
+            { customers.map(customer => {return <tr>
               <td><p>{customer.name}</p></td>
               {customer.membership_status === 'Member' ? <td><p className="green">{customer.membership_status}</p></td> : <td><p className="red">{customer.membership_status}</p></td>}
-              <td><p>{customer.customer_for_years}</p></td>
+              <td>
+                <p>{customer.customer_for_years}</p></td>
               <td>{customer.top_products.map(product => {return <p>{product}</p>})}</td>
             </tr>})}
           </tbody>
         </table>
       </div>
+    )
+  }
+}
+
+class ProductsTable extends React.Component {
+  render () {
+    let products = data.products;
+    return (
       <div>
         <table>
           <thead>
@@ -47,6 +57,18 @@ function App(props) {
           </tbody>
         </table>
       </div>
+    )
+  }
+}
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Ronzos WS</p>
+      </header>
+        <CustomersTable/>
+        <ProductsTable/>
 
       <style jsx>
         {`
